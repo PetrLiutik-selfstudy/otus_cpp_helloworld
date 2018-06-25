20bad5c36240f0e018f7286d533466369538ec2e
Petr <petr_lutik@mail.ru>
2018-06-26 02:52:19 +0400

    Начал добавлять тест

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-	std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, World!" << std::endl;
   std::cout << "Version: " << ver_major() << "." << ver_minor() << "." << ver_patch() << std::endl;
-	return 0;
+  return 0;
 }
