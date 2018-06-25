79d93be7b5539e97c37f93ff5223114ddf40b126
Petr <petr_lutik@mail.ru>
2018-06-26 02:16:28 +0400

    Добавлен вывод версии.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "ver_lib.h"
 
 int main(int argc, char const *argv[])
 {
 	std::cout << "Hello, World!" << std::endl;
+  std::cout << "Version: " << ver_major() << "." << ver_minor() << "." << ver_patch() << std::endl;
 	return 0;
 }
