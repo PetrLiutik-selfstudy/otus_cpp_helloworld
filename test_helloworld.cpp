a088a9b219d6b4178b70595358d2e842e20077b6
Petr <petr_lutik@mail.ru>
2018-06-26 04:20:25 +0400

    Заменил тесты

--- test_helloworld.cpp
+++ test_helloworld.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "gtest/gtest.h"
+#include "ver_lib.h"
 
-TEST(sample_test_case, sample_test)
+TEST(ver_test_case, ver_major_test)
 {
-  EXPECT_EQ(1, 1);
+  EXPECT_GE(ver_major(), 1);
+}
+
+TEST(ver_test_case, ver_minor_test)
+{
+  EXPECT_GE(ver_minor(), 1);
+}
+
+TEST(ver_test_case, ver_patch_test)
+{
+  EXPECT_GE(ver_patch(), 1);
 }
